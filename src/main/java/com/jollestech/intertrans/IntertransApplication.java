e438473c4ac2cf8867e548a0827e369001f699e3
Ethan Jolles <erjolles@gmail.com>
2024-09-26 12:51:55 -0400

    Init

---
+++ src/main/java/com/jollestech/intertrans/IntertransApplication.java
@@ -0,0 +1,13 @@
+package com.jollestech.intertrans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntertransApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntertransApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jollestech/intertrans/IntertransApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jollestech.intertrans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntertransApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

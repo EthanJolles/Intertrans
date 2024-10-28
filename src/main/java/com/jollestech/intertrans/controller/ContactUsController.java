72abf8a2ade0bf8d80624b693acaca33ece1e0b2
Ethan Jolles <erjolles@gmail.com>
2024-10-28 19:43:57 -0400

    Add contact page

--- src/main/java/com/jollestech/intertrans/controller/ContactUsController.java
+++ src/main/java/com/jollestech/intertrans/controller/ContactUsController.java
@@ -1,7 +1,13 @@
 package com.jollestech.intertrans.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ContactUsController {
+
+    @GetMapping("/contact")
+    public String getContactUsPage() {
+        return "/contact";
+    }
 }
